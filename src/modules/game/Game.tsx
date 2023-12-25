b732cfcef2939eebeae838bd5b66aaeb2c56b7fe
fkuca <fkuca91@gmail.com>
2023-12-25 12:26:30 +0100

    feat(BOARD-3): add DrawnGame component

--- src/modules/game/Game.tsx
+++ src/modules/game/Game.tsx
@@ -1,12 +1,25 @@
+import { useGameRandomizer } from "../../context/useGameRandomizer";
 import { Card } from "../../components/Card/Card";
+import { DrawnGame } from "./components/DrawnGame";
+
+import styles from "./Game.module.css";
 
 // https://www.codementor.io/@zmitry/react-app-structure-by-feature-guidelines-10wnfwp9eh
 export const Game = () => {
+  const { randomizedGame } = useGameRandomizer();
+
   return (
     <Card>
-      <Card.Header>Game</Card.Header>
-      <Card.Body>Lorem ipsum dolor sit amet.</Card.Body>
-      <Card.Footer></Card.Footer>
+      <Card.Header>{randomizedGame ? randomizedGame.name : "Game"}</Card.Header>
+      <Card.Body>
+        <div className={styles.game__wrapper}>
+          {randomizedGame ? (
+            <DrawnGame game={randomizedGame} />
+          ) : (
+            <>No game has been drawn yet.</>
+          )}
+        </div>
+      </Card.Body>
     </Card>
   );
 };
